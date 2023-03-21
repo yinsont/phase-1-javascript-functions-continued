aa57232cc9cd86aa21c98fb9ca650d5fc42e6c27
Yinson Tso <yinson.tso@gmail.com>
2023-03-21 10:51:45 -0400

    fubctions continued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (activity) {
+    if (activity === undefined) activity = 'roller-skate'
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork (activity) {
+    if (activity === undefined) activity = 'go to the office'
+    return (`This Monday, I will ${activity}.`)
+}   
+
+function wrapAdjective (highlight){
+    return function(string){
+        return (`You are ${highlight}${string}${highlight}!`)
+    }
+}
